9ac99b67e1d12d4e8c2e52aaf7c24782401c80d7
Archiep05 <aap30@kent.ac.uk>
2026-03-25 18:25:35 +0000

    class that will store existing exercises and then new ones added by the user

---
+++ utils/gym_library.py
@@ -0,0 +1,23 @@
+
+
+
+class MuscleGroup:
+
+    def __init__(self, name, exercises):
+        self.name = name
+        self.exercises = exercises
+    
+    def get_exercises(self):
+        return self.exercises
+    
+    def add_custom_exercises(self, exercise_name)
+        self.exercises.append(exercise_name)
+    
+    def __str__(self):
+        return f"{self.name} ({len(self.exercises)} exercises)"
+
+       
+
+
+
+
