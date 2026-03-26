7c2cb6caa2bdaf3463f009d0fcdadb4969d0aa6a
Archiep05 <aap30@kent.ac.uk>
2026-03-26 14:13:03 +0000

    added all groups list to call from later

--- utils/gym_library.py
+++ utils/gym_library.py
@@ -10,14 +10,32 @@ class MuscleGroup:
     def get_exercises(self):
         return self.exercises
     
-    def add_custom_exercises(self, exercise_name)
+    def add_custom_exercises(self, exercise_name):
         self.exercises.append(exercise_name)
     
     def __str__(self):
         return f"{self.name} ({len(self.exercises)} exercises)"
 
 CHEST = MuscleGroup("Chest" ["Barbell Bench Press" "Barbell Bench Press","Dumbbell Bench Press",
-    "Incline Barbell Bench Press","Incline Dumbbell Bench Press","Chest Fly","Push Up"])
+    "Incline Barbell Bench Press",
+    "Incline Dumbbell Bench Press","Chest Fly","Push Up"])
+
+BACK = MuscleGroup("Back", ["Deadlift","Barbell Row","Dumbbell Row","Lat Pulldown",
+  "Pull Up","Seated Cable Row"])
+
+BICEPS = MuscleGroup("Biceps", ["Barbell Curl","Dumbbell Curl",
+   "Hammer Curl","Preacher Curl",])
+
+TRICEPS = MuscleGroup("Triceps", ["Tricep Pushdown","Skull Crusher","Overhead Tricep Extension",
+"Dips","Close Grip Bench Press"])
+
+SHOULDERS = MuscleGroup("Shoulders", ["Overhead Press","Dumbbell Lateral Raise","Front Raise",
+"Arnold Press","Face Pull"])
+
+LEGS = MuscleGroup("Legs", ["Barbell Squat","Leg Press","Bulgarian Split Squat",
+"Leg Curl","Leg Extension","Calf Raise"])
+
+ALL_GROUPS = [CHEST, BACK, BICEPS, TRICEPS, SHOULDERS, LEGS,]
 
 
        
